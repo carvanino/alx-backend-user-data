e6c2612f79a0e8a836ea80409c6e2f729c99a896
Akinola <akinolatofunmi04@gmail.com>
2023-05-29 19:45:34 +0000

    None conditonals

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,10 +20,10 @@ class Auth:
         """
         Returns False
         """
-        # if path is None:
-        #     return True
-        # if excluded_paths is None or len(excluded_paths) == 0:
-        #     return True
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
         for excluded_path in excluded_paths:
             if path.rstrip('/') == excluded_path.rstrip('/'):
                 return False
