bd84a91de16cba3fff27ebdb369ee493dc05d462
Akinola <akinolatofunmi04@gmail.com>
2023-05-29 17:24:51 +0100

    Task 9 - Basic - User Credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Contains the sub class BasicAuth, inherited from Auth
 
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -25,4 +26,22 @@ class BasicAuth(Auth):
                 str) and authorization_header.startswith('Basic '):
             encoded = authorization_header.split()
             return encoded[1]
+        return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes a Base64 string
+        Args:
+            base64_authorization_header (str): A Base64 String to be decoded
+        Returns:
+            (str): The decoded value of base64_authorization_header
+        """
+        if base64_authorization_header and isinstance(
+                base64_authorization_header, str):
+            try:
+                decoded_string = base64.b64decode(base64_authorization_header)
+            except base64.binascii.Error:
+                return None
+            return decoded_string.decode('utf-8')
         return None
