3bb3a3e1b3c543ae7e46fc85cf0c58eeb0bbd792
Akinola <akinolatofunmi04@gmail.com>
2023-05-29 16:49:17 +0100

    Task 6 - 6. Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Contains the sub class BasicAuth, inherited from Auth
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    The BasicAuth class
+    """
+    pass
