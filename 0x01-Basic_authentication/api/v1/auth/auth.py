aa3d894587508fe14faa82ea81f1c0748370784b
Akinola <akinolatofunmi04@gmail.com>
2023-05-29 15:08:27 +0100

    Refactor conditional statement for require_auth()

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,16 +20,14 @@ class Auth:
         """
         Returns False
         """
-        if path is not None and path[-1] != "/":
-            path = path + '/'
         if path is None:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        if path in excluded_paths:
-            return False
-        if path not in excluded_paths:
-            return True
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
+        return True
 
     def authorization_header(self, request: request = None) -> str:
         """
