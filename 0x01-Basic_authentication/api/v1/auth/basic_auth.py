07685a728d5f22a3c8f4f61c9f9cfc66a82b4482
Akinola <akinolatofunmi04@gmail.com>
2023-05-29 17:05:43 +0100

    Task 7 - Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,18 @@ class BasicAuth(Auth):
     """
     The BasicAuth class
     """
-    pass
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extracts the endoded credentials from the authorization header
+        """
+        if authorization_header and isinstance(
+                authorization_header,
+                str) and authorization_header.startswith('Basic '):
+            encoded = authorization_header.split()
+            return encoded[1]
+        return None
