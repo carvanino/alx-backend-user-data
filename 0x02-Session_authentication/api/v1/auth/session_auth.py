471e226f45748e4a4fcd658df736347dd986a4d0
Akinola <akinolatofunmi04@gmail.com>
2023-05-31 19:29:48 +0100

    Task 2 - Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,13 +4,28 @@ Holds the sub class SessionAuth from Auth
 """
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     A Session Authentication mechanism
     """
+    user_id_by_session_id = {}
 
     def __init__(self) -> None:
         super().__init__()
         pass
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for an existing user
+        Args:
+            user_id (str): ID of a user
+        Return (str): The session ID of the user
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
