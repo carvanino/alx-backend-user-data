5039f7e945c2618e265810310669fb64695bbb90
Akinola <akinolatofunmi04@gmail.com>
2023-06-05 14:00:06 +0100

    Task 5 - Register User

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,8 +3,11 @@
 Defines a function that hashes a password
 """
 
-
+from db import DB
+from sqlalchemy.exc import NoResultFound
+from user import User
 from bcrypt import hashpw, gensalt
+
 
 def _hash_password(password: str) -> bytes:
     """
@@ -16,3 +19,24 @@ def _hash_password(password: str) -> bytes:
     """
     hashed_password = hashpw(password.encode('utf-8'), gensalt())
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Implements a Sign_up feature
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+        else:
+            raise ValueError("User {} already exists".format(email))
+        return new_user
