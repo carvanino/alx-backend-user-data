1da975783a720175fdc5a0d113b07f05de597bf0
Akinola <akinolatofunmi04@gmail.com>
2023-06-05 20:57:40 +0100

    Task 18 - Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -90,7 +90,7 @@ class Auth:
         except Exception:
             return None
         return user
-    
+
     def destroy_session(self, user_id: int) -> None:
         """
         Implements a Logout feature, by deleting users session
@@ -101,7 +101,7 @@ class Auth:
             return None
         self._db.update_user(user_id, session_id=None)
         return None
-    
+
     def get_reset_password_token(self, email: str) -> str:
         """
         Generate a reset token for password
@@ -114,3 +114,14 @@ class Auth:
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Resets and update a users password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except ValueError:
+            raise ValueError
+        hashed = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed, reset_token=None)
